d50658a73e834d88ee25acb6ed5d379bf65a5466
Rancho <72544580+RanchoTao@users.noreply.github.com>
2026-05-17 00:07:42 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Visual-Deadline/',
   plugins: [react(), tailwindcss()],
 });
